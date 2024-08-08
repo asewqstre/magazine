b2c86bfe293d185afd9c76ad715585083818db92
Aziz <aziz.mutallap.04az@gmail.com>
2024-08-08 15:08:42 +0500

    created and registered in admin Article model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField('Название',max_length=150)
+    content = models.TextField('Содержание')
+    creation_datetime = models.DateTimeField('Дата и время создания', default=timezone.now)
+    publication_datetime = models.DateTimeField('Дата и время публикации')
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = "Статья"
+        verbose_name_plural = "Статьи"
